461d377e6fccf5ce6c72ddb9e9f7328dc3c6f4b6
Jimmy Tang <blay.tenshi@gmail.com>
2019-04-05 19:11:44 +1100

    Refactored stuff. Moved out of bounds coordinates check into Canvas so there's less messy code in Canvas Editor. Reordered Command Module to check length of command arguments first before parsing each argument. Refactored some tests just in case to ensure against empty canvases. Errors more understandable.

--- exceptions/InvalidCoordinateError.js
+++
@@ -1,8 +0,0 @@
-class InvalidCoordinateError extends Error {
-    constructor(x, y) {
-        super(`Coordinate values cannot be less than 0! { X: ${x}, Y: ${y} }`);
-        this.name = "InvalidCoordinateError";
-    }
-}
-
-module.exports = InvalidCoordinateError;
